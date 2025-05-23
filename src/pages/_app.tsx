3c24f3cd5a89dbfb619b248c59169f2928a0d721
EdPsych Connect AI <edpsychconnect@example.com>
2025-05-23 12:27:36 -0400

    Enhance i18n framework with multilingual support and automated testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,18 +5,21 @@ import { Navbar } from '@/components/layout/Navbar';
 import { Footer } from '@/components/layout/Footer';
 import { CookieConsentProvider } from '@/components/legal/CookieConsentProvider';
 import { Toaster } from '@/components/ui/toaster';
+import { I18nProvider } from '@/lib/i18n';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <CookieConsentProvider>
-        <Navbar />
-        <main className="min-h-screen">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-        <Toaster />
-      </CookieConsentProvider>
+      <I18nProvider>
+        <CookieConsentProvider>
+          <Navbar />
+          <main className="min-h-screen">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+          <Toaster />
+        </CookieConsentProvider>
+      </I18nProvider>
     </SessionProvider>
   );
 }
