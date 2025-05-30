ad0c0cec718ef589daa27d8303c7225aa56eaa45
EdPsych Connect Developer <edpsych.connect@example.com>
2025-05-29 23:09:52 -0400

    Fix premium design: Import brand.css and enhanced-brand.css in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-// Minimal _app.js with only essential CSS
+// Import premium CSS files for the enhanced design
 import '../src/styles/enhanced-globals.css';
+import '../src/styles/brand.css';
+import '../src/styles/enhanced-brand.css';
+import { ThemeProvider } from 'next-themes';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
